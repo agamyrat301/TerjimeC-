7357b2accf0b1f6a450a02d217fb15e8483d717d
agamyrat301 <agajan301@gmail.com>
2020-10-14 01:53:19 +0500

    fuck

--- Terjime/Program.cs
+++ Terjime/Program.cs
@@ -12,86 +12,54 @@ namespace Terjime
             string[] row = new string[4];
             int num = 0;
 
-            MySqlConnection dbConn = new MySqlConnection("server=localhost;database=durdysese;uid=root;pwd=;");
+            MySqlConnection dbConn = new MySqlConnection("server=localhost;database=fromdurdy;uid=root;pwd=;");
             dbConn.Open();
             var cmd = new MySqlCommand("select ID,QUESTION_TK,ANSWER_1_TK,ANSWER_2_TK,ANSWER_3_TK,ANSWER_4_TK " +
-                "from questions where ID = 474242", dbConn);
+                "from questions", dbConn);
             var reader = cmd.ExecuteReader();
-
 
             while (reader.Read())
             {
+                row[num] = reader.GetString(0).Trim() + "<r>" + reader.GetString(1) + "<r>" +
+                    reader.GetString(2) + "<r>" + reader.GetString(3) + "<r>" + reader.GetString(4) + "<r>" + reader.GetString(5) + "<r>" + "<wroww>";
 
+                if (num == 3)
+                {
+                    string respbody = TranslateQuestion("tk", "ru", row[0] + row[1] + row[2] + row[3]);
+                    string[] MyArr = respbody.Split(new string[] { "<wroww>" }, StringSplitOptions.None);
+                        foreach (var arr in MyArr)
+                        {
+                            string[] yoww = arr.Split(new string[] { "<r>" }, StringSplitOptions.None);
+                            if (yoww.Length > 5)
+                                UpdateQuestion(yoww[0], yoww[1], yoww[2], yoww[3], yoww[4], yoww[5]);
+                        }
+                    num = 0;
 
-              string target = reader.GetString(0) + "<r>" + reader.GetString(1) + "<r>" + 
-                    reader.GetString(2) + "<r>" + reader.GetString(3) + "<r>" + reader.GetString(4) + "<r>" + reader.GetString(5);
-
-                Console.WriteLine(target);
-
-                //string respbody = TranslateQuestion("tk", "ru", target);
-                //Console.WriteLine(respbody);
-
-
-                //row[num] = reader.GetString(0).Trim() + "<r>" + reader.GetString(1) + "<r>" +
-                //     reader.GetString(2) + "<r>" + reader.GetString(3) + "<r>" + reader.GetString(4) + "<r>" + reader.GetString(5) + "<wroww>";
-
-
-
-
-                //if (num == 3)
-                //{
-
-                //    string respbody = TranslateQuestion("tk", "ru", row[0] + row[1] + row[2] + row[3]);
-
-                //    Console.WriteLine(respbody);
-                //    string[] MyArr = respbody.Split("<wroww>");
-
-                //    foreach (var arr in MyArr)
-                //    {
-                //        string[] yoww = arr.Split("<r>");
-                //        Console.WriteLine(yoww[0]);
-                //    }
-
-
-                //    num = 0;
-
-                //}
-
-
-                //num++;
-
-
+                }
+                num++;
             }
             Console.ReadKey();
-
         }
-
-
-
 
         private static void UpdateQuestion(string id, string question, string answer_1, string answer_2, string answer_3, string answer_4)
         {
-
             using (MySqlConnection con = new MySqlConnection("server=localhost;database=fromdurdy;uid=root;pwd=;"))
             {
-                string Upquery = "UPDATE questions SET QUESTION_TK=@question, ANSWER_1_TK=@answer_1, " +
-                "ANSWER_2_TK=@answer_2,ANSWER_3_TK=@answer_3, ANSWER_4_TK=@answer_4 WHERE id=@id";
+                string Upquery = "UPDATE questions SET QUESTION_RU=@question, ANSWER_1_RU=@answer_1, ANSWER_2_RU=@answer_2,ANSWER_3_RU=@answer_3, ANSWER_4_RU=@answer_4 WHERE ID=@id";
                 con.Open();
                 MySqlCommand newCmd = new MySqlCommand();
                 newCmd.CommandText = Upquery;
                 newCmd.Connection = con;
-                newCmd.Parameters.AddWithValue("@id", id);
-                newCmd.Parameters.AddWithValue("@question_tm", question);
-                newCmd.Parameters.AddWithValue("@answer_1_tm", answer_1);
-                newCmd.Parameters.AddWithValue("@answer_2_tm", answer_2);
-                newCmd.Parameters.AddWithValue("@answer_3_tm", answer_3);
-                newCmd.Parameters.AddWithValue("@answer_4_tm", answer_4);
+                newCmd.Parameters.AddWithValue("@id", id.Trim());
+                newCmd.Parameters.AddWithValue("@question", question);
+                newCmd.Parameters.AddWithValue("@answer_1", answer_1);
+                newCmd.Parameters.AddWithValue("@answer_2", answer_2);
+                newCmd.Parameters.AddWithValue("@answer_3", answer_3);
+                newCmd.Parameters.AddWithValue("@answer_4", answer_4);
                 newCmd.ExecuteNonQuery();
                 con.Close();
             }
         }
-
-
 
         public static String TranslateQuestion(string langFrom, string langTo, string AnswerTxt)
         {
@@ -107,11 +75,7 @@ namespace Terjime
             var resultBody = response.Content.ReadAsStringAsync().Result;
             return resultBody;
 
-
         }
-
-
-
 
     }
 }
